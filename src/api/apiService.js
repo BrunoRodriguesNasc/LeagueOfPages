ce26fe6e472d5605bd6e60694b3fd9f42f4f3407
Bruno Rodrigues <bruno.rodrigues@ahgora.com.br>
2022-02-11 23:35:16 -0300

    Add path da api

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getChampion = async (champion) => {
 
     const { REACT_APP_BASE_URL } = process.env;  
-    const { data } = await axios.get(REACT_APP_BASE_URL, champion);
+    const { data } = await axios.get('https://backendlolzin.herokuapp.com/champions/', champion);
     return data;
 }
 
